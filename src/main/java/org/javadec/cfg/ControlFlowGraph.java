2d83e8fc23d35f2e3be21bc5457a02e2430bba7b
Andrey Ponomarev <ponomandr@users.noreply.github.com>
2014-09-28 01:14:21 +0200

    Extracted ControlFlowGraph builder

--- src/main/java/org/javadec/cfg/ControlFlowGraph.java
+++ src/main/java/org/javadec/cfg/ControlFlowGraph.java
@@ -6,82 +6,109 @@ import org.objectweb.asm.tree.InsnList;
 import org.objectweb.asm.tree.JumpInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 
 import static org.javadec.InsnUtils.*;
 
 public class ControlFlowGraph {
-    final Map<AbstractInsnNode, BasicBlock> blockMap = new LinkedHashMap<>();
-    private final MethodNode methodNode;
-    BasicBlock root;
-    private InsnList insnList;
 
-    public ControlFlowGraph(MethodNode methodNode) {
-        this.methodNode = methodNode;
-        insnList = methodNode.instructions;
-        identifyBlocks();
-        connectBlocks();
+    public final BasicBlock root;
+    private final String name;
+
+    public ControlFlowGraph(String name, BasicBlock root) {
+        this.name = name;
+        this.root = root;
     }
 
-    private void identifyBlocks() {
-        root = startBlock(0);
-
-        BasicBlock block = root;
-        for (int i = 0; i < insnList.size(); i++) {
-            AbstractInsnNode insn = insnList.get(i);
-            if (isLabel(insn)) {
-                block = startBlock(i);
-            } else if (isJump(insn) || isReturn(insn) || isThrow(insn)) {
-                block.end = i;
-                block = startBlock(i + 1);
-            } else {
-                block.end = i;
-            }
-        }
-    }
-
-    private void connectBlocks() {
-        for (BasicBlock basicBlock : blockMap.values()) {
-            AbstractInsnNode endInsn = insnList.get(basicBlock.end);
-            if (endInsn instanceof JumpInsnNode) {
-                JumpInsnNode jump = (JumpInsnNode) endInsn;
-                basicBlock.addSucc(blockMap.get(jump.label));
-            }
-            if (!isGoto(endInsn) && !isReturn(endInsn) && !isThrow(endInsn)) {
-                basicBlock.addSucc(blockMap.get(endInsn.getNext()));
-            }
-        }
-    }
-
-    private BasicBlock startBlock(int startIndex) {
-        if (startIndex >= insnList.size()) {
-            return null;
-        }
-
-        AbstractInsnNode insn = insnList.get(startIndex);
-        BasicBlock block = blockMap.get(insn);
-        if (block != null) {
-            return block;
-        }
-
-        block = new BasicBlock(startIndex);
-        blockMap.put(insn, block);
-        return block;
+    public static ControlFlowGraph create(MethodNode methodNode) {
+        return new Builder(methodNode).build();
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("digraph ").append(methodNode.name).append(" {\n");
-        for (BasicBlock bb : blockMap.values()) {
-            for (BasicBlock succ : bb.succ) {
-                sb.append("  b").append(bb.start).append(" -> b").append(succ.start).append("\n");
-            }
-            sb.append("  b").append(bb.start).append("[shape=box,label=\"").append(bb).append("\"];\n");
-        }
+        sb.append("digraph ").append(name).append(" {\n");
+        appendBlock(sb, root, new HashSet<>());
         sb.append("}\n");
         return sb.toString();
+    }
+
+    private void appendBlock(StringBuilder sb, BasicBlock bb, Set<BasicBlock> visited) {
+        if (!visited.add(bb)) return;
+
+        for (BasicBlock succ : bb.succ) {
+            sb.append("  b").append(bb.start).append(" -> b").append(succ.start).append("\n");
+            appendBlock(sb, succ, visited);
+        }
+        sb.append("  b").append(bb.start).append("[shape=box,label=\"").append(bb).append("\"];\n");
+    }
+
+
+    private static class Builder {
+        private final InsnList insnList;
+        private final Map<AbstractInsnNode, BasicBlock> blockMap = new LinkedHashMap<>();
+        private final MethodNode methodNode;
+        private BasicBlock root;
+
+        public Builder(MethodNode methodNode) {
+            this.methodNode = methodNode;
+            insnList = methodNode.instructions;
+        }
+
+        public ControlFlowGraph build() {
+            identifyBlocks();
+            connectBlocks();
+            return new ControlFlowGraph(methodNode.name, root);
+        }
+
+        private BasicBlock startBlock(int startIndex) {
+            if (startIndex >= insnList.size()) {
+                return null;
+            }
+
+            AbstractInsnNode insn = insnList.get(startIndex);
+            BasicBlock block = blockMap.get(insn);
+            if (block != null) {
+                return block;
+            }
+
+            block = new BasicBlock(startIndex);
+            blockMap.put(insn, block);
+            return block;
+        }
+
+        private void connectBlocks() {
+            for (BasicBlock basicBlock : blockMap.values()) {
+                AbstractInsnNode endInsn = insnList.get(basicBlock.end);
+                if (endInsn instanceof JumpInsnNode) {
+                    JumpInsnNode jump = (JumpInsnNode) endInsn;
+                    basicBlock.addSucc(blockMap.get(jump.label));
+                }
+                if (!isGoto(endInsn) && !isReturn(endInsn) && !isThrow(endInsn)) {
+                    basicBlock.addSucc(blockMap.get(endInsn.getNext()));
+                }
+            }
+        }
+
+        private void identifyBlocks() {
+            root = startBlock(0);
+
+            BasicBlock block = root;
+            for (int i = 0; i < insnList.size(); i++) {
+                AbstractInsnNode insn = insnList.get(i);
+                if (isLabel(insn)) {
+                    block = startBlock(i);
+                } else if (isJump(insn) || isReturn(insn) || isThrow(insn)) {
+                    block.end = i;
+                    block = startBlock(i + 1);
+                } else {
+                    block.end = i;
+                }
+            }
+        }
     }
 }
 
--- src/test/java/org/javadec/cfg/ControlFlowGraphTest.java
+++ src/test/java/org/javadec/cfg/ControlFlowGraphTest.java
@@ -3,6 +3,7 @@ package org.javadec.cfg;
 import org.javadec.ClassFileReader;
 import org.junit.Test;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
 
 import static org.javadec.TestHelper.compileResource;
 import static org.javadec.TestHelper.disassemble;
@@ -14,8 +15,9 @@ public class ControlFlowGraphTest {
         byte[] bytes = compileResource("sample.HelloWorld", "/sample/HelloWorld.java");
         System.out.println(disassemble(bytes));
         ClassNode classNode = ClassFileReader.readClass(bytes);
+        MethodNode methodNode = classNode.methods.get(1);
 
-        ControlFlowGraph graph = new ControlFlowGraph(classNode.methods.get(1));
+        ControlFlowGraph graph = ControlFlowGraph.create(methodNode);
         System.out.println(graph);
 
         System.out.println();
